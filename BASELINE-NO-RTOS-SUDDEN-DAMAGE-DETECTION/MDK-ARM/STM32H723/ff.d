f47247177296996a311a2a44aa7dc1a11ac526aa
Shuaiwen-Cui <SHUAIWEN001@e.ntu.edu.sg>
2024-04-30 18:55:34 +0800

    auto commit by win-upload.bat

---
+++ BASELINE-NO-RTOS-SUDDEN-DAMAGE-DETECTION/MDK-ARM/STM32H723/ff.d
@@ -0,0 +1,6 @@
+stm32h723\ff.o: ..\FatFs\ff.c
+stm32h723\ff.o: ..\FatFs\ff.h
+stm32h723\ff.o: ..\FatFs\integer.h
+stm32h723\ff.o: ..\FatFs\ffconf.h
+stm32h723\ff.o: ..\FatFs\diskio.h
+stm32h723\ff.o: D:\Keil5\ARM\ARMCC\Bin\..\include\stdarg.h
---
+++ BASELINE-NO-RTOS-SUDDEN-DAMAGE-DETECTION/User/damage_detection_data.h
@@ -0,0 +1,30 @@
+#ifndef __DAMAGE_DETECTION_DATA_H
+#define __DAMAGE_DETECTION_DATA_H
+
+#include "sys.h"
+#include "delay.h"
+#include "usart.h" 
+#include "led.h"
+#include "key.h"
+#include "ltdc.h"
+#include "lcd.h"
+#include "sdram.h"
+#include "usmart.h"
+#include "pcf8574.h"
+#include "mpu.h"
+#include "malloc.h"
+#include "w25qxx.h"
+#include "sdmmc_sdcard.h"
+#include "timer.h"
+#include "arm_math.h"
+
+float32_t signal[] = {0};
+
+
+
+
+
+
+
+
+#endif /* __DAMAGE_DETECTION_DATA_H */
